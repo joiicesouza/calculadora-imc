23e5fffed6c26965513add4288c9df06f796ef78
joice <joice1998@gmail.com>
2022-08-05 21:00:49 -0300

    meu projeto de calculadora imc

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function start() {
+  var buttonCalculateImc = document.querySelector('#button-calculate-imc');
+  buttonCalculateImc.addEventListener('click', handleButtonClick);
+
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  inputWeight.addEventListener('input', handleButtonClick);
+  inputHeight.addEventListener('input', handleButtonClick);
+  handleButtonClick();
+}
+
+function calculateImc(weight, height) {
+  return weight / (height * height);
+}
+
+function handleButtonClick() {
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  var weight = Number(inputWeight.value);
+  var height = Number(inputHeight.value);
+  var imcResult = document.querySelector('#imc-result');
+
+  var imc = calculateImc(weight, height);
+  var formattedImc = imc.toFixed(2).replace('.', ',');
+
+  imcResult.textContent = formattedImc;
+}
+
+start();
